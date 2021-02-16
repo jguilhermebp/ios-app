b211c04cca0087f14b16ef26edfec1e38d823da0
“José <jguilhermebp@live.com>
2021-02-15 22:50:29 -0300

    Initial Commit

---
+++ Shared/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  Shared
+//
+//  Created by Jose Guilherme on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
